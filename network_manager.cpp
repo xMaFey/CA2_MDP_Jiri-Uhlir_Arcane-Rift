ae188ffcbccb426d1755f0bf565ccba6835f9cf8
xMaFey <uhlir2002@seznam.cz>
2026-04-12 17:39:02 +0200

    Debug packet size

--- network_manager.cpp
+++ network_manager.cpp
@@ -5,6 +5,7 @@
 
 #include "network_manager.hpp"
 #include <algorithm>
+#include <iostream>
 
 namespace
 {
@@ -614,6 +615,10 @@ bool NetworkManager::send_world_state_to_player(int player_id, const WorldStateP
 
         sf::Packet packet = make_typed_packet(PacketType::WorldState);
         packet << state;
+
+        // DEBUG: print the real serialized packet size in bytes.
+        std::cout << "[SERVER] WorldState packet to player " << player_id
+            << ": " << packet.getDataSize() << " bytes\n";
 
         return client.socket->send(packet) == sf::Socket::Status::Done;
     }
